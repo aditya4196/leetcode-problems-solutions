325b93eea221ea53d46c3333e634460a7f164482
Aditya Jadhav <32817064+aditya4196@users.noreply.github.com>
2023-11-12 13:55:44 -0500

    Time: 78 ms (7.17%), Space: 43.8 MB (57.56%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,22 +1,20 @@
 class Solution {
     public int change(int amount, int[] coins) {
+        Integer[][] dp = new Integer[coins.length+1][amount+1];
+        return helper(amount, 0, coins, dp);
+    }
+    
+    public int helper(int amount, int idx, int[] coins, Integer[][] dp){
+        if(amount == 0) return 1;
+        if(idx >= coins.length) return 0;
+        if(dp[idx][amount]!=null) return dp[idx][amount];
+        int totalWays = 0;
         
-        int[] dp = new int[amount+1];
-        dp[0] = 1;
-        
-        for(int coin : coins){
-            for(int i=0; i<=amount; i++){
-                if(i < coin) continue;
-                dp[i] += dp[i-coin];
-            }
+        for(int i=idx; i<coins.length; i++){
+            if(coins[i] > amount) continue;
+            totalWays += helper(amount-coins[i], i, coins, dp);
         }
         
-        return dp[amount];
+        return dp[idx][amount] = totalWays;
     }
 }
-
-/*
-
-
-
-*/
