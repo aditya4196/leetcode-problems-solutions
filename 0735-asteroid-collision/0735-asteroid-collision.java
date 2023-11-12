6098a97db644e4bfb1866fce8b740a783539444e
Aditya Jadhav <32817064+aditya4196@users.noreply.github.com>
2023-11-12 15:00:26 -0500

    Time: 4 ms (91.65%), Space: 44.6 MB (12.05%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.java
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -1,48 +1,37 @@
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
-        
         Deque<Integer> stack = new LinkedList();
-        int i=0;
-        while(i < asteroids.length){
-            if(asteroids[i] < 0 && !stack.isEmpty() && stack.peek() > 0){
-                if(stack.peek() < (-1*asteroids[i])){
-                    stack.pop();
-                }
-                else if(stack.peek() == (-1*asteroids[i])){
-                    stack.pop();
-                    i++;
+        
+        int idx = 0;
+        while(idx < asteroids.length){
+            int curr = asteroids[idx];
+            if(curr < 0 && !stack.isEmpty() && stack.peek() > 0){
+                    if(stack.peek() > -1*curr){
+                        idx++;
+                    }
+                    else if(stack.peek() < -1*curr){
+                        stack.pop();
+                    }
+                    else{
+                        idx++;
+                        stack.pop();
+                    }
                 }
                 else{
-                    i++;
+                    stack.push(curr);
+                    idx++;
                 }
-            }
-            else{
-                stack.push(asteroids[i]);
-                i++;
-            }
         }
         
         int[] result = new int[stack.size()];
-        int j=stack.size()-1;
-        for(int val : stack){
-         result[j--] = val;   
+        
+        int i=stack.size()-1;
+        while(!stack.isEmpty()){
+            result[i--] = stack.pop();
         }
         
         return result;
         
         
-        
     }
 }
-
-/*
-
-[5, 10, -5]
-
-collision - 
-if curr is neg 
-    while stack.peek() is positive abs(stack.peek()) < abs()
-
-
-
-*/
