3d454b98639955b7b0b8c078d89879906060581f
Aditya Jadhav <32817064+aditya4196@users.noreply.github.com>
2023-11-26 15:34:08 -0500

    Time: 2 ms (95.52%), Space: 40.5 MB (59.56%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,13 +1,11 @@
 class Solution {
     public int countSubstrings(String s) {
-        
         int totalPalindromeSubstrings = 0;
         for(int i=0; i<s.length()-1; i++){
             totalPalindromeSubstrings += getPalindromeCount(s, i, i);
             totalPalindromeSubstrings += getPalindromeCount(s, i, i+1);
         }
         return totalPalindromeSubstrings+1;
-        
     }
     
     public static int getPalindromeCount(String s, int i, int j){
@@ -17,7 +15,6 @@ class Solution {
             i--;
             j++;
         }
-        
         return palindromeCount;
     }
 }
