f42296b07a298d1cee2624c86cd662b5f9447f03
Aditya Jadhav <32817064+aditya4196@users.noreply.github.com>
2023-11-29 21:42:20 -0500

    Time: 20 ms (36.82%), Space: 44.5 MB (61.46%) - LeetHub

--- 1630-arithmetic-subarrays/1630-arithmetic-subarrays.java
+++ 1630-arithmetic-subarrays/1630-arithmetic-subarrays.java
@@ -2,11 +2,9 @@ class Solution {
     public List<Boolean> checkArithmeticSubarrays(int[] nums, int[] l, int[] r) {
         
         List<Boolean> result = new ArrayList<Boolean>();
-        
         for(int i=0; i<l.length; i++){
             int[] tempArr = Arrays.copyOfRange(nums, l[i], r[i]+1);
             Arrays.sort(tempArr);
-            
             int diff = 0;
             boolean valid = true;
             for(int j=0; j<tempArr.length-1; j++){
@@ -14,9 +12,7 @@ class Solution {
                     valid = false;
                     break;
                 }
-                else{
-                    diff = tempArr[j+1] - tempArr[j];
-                }
+                else diff = tempArr[j+1] - tempArr[j];
             }
             if(valid) result.add(true);
             else result.add(false);
@@ -29,7 +25,15 @@ class Solution {
 
 /*
 
-1, 3, 5, 7, 9
+complexity = O(queriescount * n * log(n))
 
+0  1. 2. 3. 4. 5
+4, 6, 5, 9, 3, 7
 
+2  1. 4. 6. 4. 0
+
+l = [0,0,2], 
+r = [2,3,5]
+
+result [true,false,true]
 */
