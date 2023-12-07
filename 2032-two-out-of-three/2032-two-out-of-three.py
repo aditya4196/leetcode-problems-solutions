70cbc41713bfbf5606a65769a8f74c71d8000a1a
Aditya Jadhav <32817064+aditya4196@users.noreply.github.com>
2023-12-06 19:54:39 -0500

    Time: 78 ms (31.01%), Space: 16.3 MB (65.18%) - LeetHub

---
+++ 2032-two-out-of-three/2032-two-out-of-three.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoOutOfThree(self, nums1: List[int], nums2: List[int], nums3: List[int]) -> List[int]:
+        set1 = set(nums1)
+        set2 = set(nums2)
+        set3 = set(nums3)
+        
+        return list((set1 & set2) | (set1 & set3) | (set2 & set3))
+        
+        
+        
+        
+        
+        
