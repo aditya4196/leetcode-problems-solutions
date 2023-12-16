c15dccd33d36cf1cb93f9adb8eb6d9ce3700b64d
Aditya Jadhav <32817064+aditya4196@users.noreply.github.com>
2023-12-15 21:12:11 -0500

    Time: 48 ms (18.54%), Space: 16.8 MB (5.91%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,24 +1,28 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         
-        def backtrack(n, opencount, closecount, innerlist):
-            if n*2 == len(innerlist):
-                output.append("".join(innerlist))
+        def createString(binstr):
+            list = []
+            for i in range(0,(n*2)):
+                list.append('(' if binstr[i] == '1' else ')')
+            return "".join(list)
+                
+                
+        def backtrack(n, opencount, closecount, num):
+            if n*2 == len(str(num)):
+                output.append(createString(str(num)))
                 return
             
             if closecount < opencount:
-                innerlist.append(')')
-                backtrack(n, opencount, closecount+1, innerlist)
-                innerlist.pop()
+                backtrack(n, opencount, closecount+1, num*10)
+                
             
             if opencount < n:
-                innerlist.append('(')
-                backtrack(n, opencount+1, closecount, innerlist)
-                innerlist.pop()
+                backtrack(n, opencount+1, closecount, num*10+1)
             
-            
+
         output = []
-        backtrack(n, 0, 0, [])
+        backtrack(n, 0, 0, 0)
         return output
         
         
