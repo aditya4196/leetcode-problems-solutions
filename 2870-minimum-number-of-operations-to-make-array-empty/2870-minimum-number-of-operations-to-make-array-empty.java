362b71390397bb68ab6c5149597fe2c1ed774926
Aditya Jadhav <32817064+aditya4196@users.noreply.github.com>
2024-01-04 16:16:53 -0500

    Time: 28 ms (24.62%), Space: 56.4 MB (80.77%) - LeetHub

---
+++ 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int minOperations(int[] nums) {
+        
+        int n = nums.length;
+        Map<Integer, Integer> countmap = new HashMap();
+        int maxCount = 0;
+        
+        for(int num : nums){
+            countmap.put(num, countmap.getOrDefault(num,0)+1);
+            maxCount = Math.max(maxCount, countmap.get(num));
+        }
+        
+        int[] freq = new int[maxCount+1];
+        int[] dp = new int[maxCount+1];
+        
+        for(int val : countmap.values()){
+            if(val == 1) return -1;
+            freq[val]+=1;
+        }
+        
+        if(maxCount == 2) return freq[2];
+        if(maxCount == 3) return freq[3] + freq[2];
+        
+        dp[1] = 1;
+        dp[2] = dp[3] = 1;
+        
+        for(int i=4; i<=maxCount; i++){
+            dp[i] = Math.min(dp[i-2],dp[i-3])+1;
+        }
+    
+        
+        int totalops = 0;
+        for(int i=0; i<freq.length; i++){
+            totalops += (dp[i]*freq[i]);
+        }
+        
+        return totalops;
+        
+    }
+}
