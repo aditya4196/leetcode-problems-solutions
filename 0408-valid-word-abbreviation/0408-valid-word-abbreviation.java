667276881a49a6b4747292d4228fe2bf78c355d4
Aditya Jadhav <32817064+aditya4196@users.noreply.github.com>
2024-01-07 07:44:02 -0500

    Time: 5 ms (11.84%), Space: 41.7 MB (18.09%) - LeetHub

---
+++ 0408-valid-word-abbreviation/0408-valid-word-abbreviation.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean validWordAbbreviation(String word, String abbr) {
+        
+        int p=0, q=0;
+        
+        while(p<word.length() && q<abbr.length()){
+            char w = word.charAt(p);
+            char a = abbr.charAt(q);
+            
+            if(w == a){
+                p++;
+                q++;
+            }
+            else if(Character.isDigit(a)){
+                int left = q;
+
+                if(a-'0' == 0) return false;
+                
+                while(q < abbr.length() && Character.isDigit(abbr.charAt(q))){
+                    q++;
+                }
+                int num = Integer.parseInt(abbr.substring(left,q));
+                
+                System.out.println(num);
+                if(num > word.length()-p) return false;
+                p+=num;
+            }
+            else return false;
+        }
+        
+        return p == word.length() && q == abbr.length();
+        
+    }
+}
