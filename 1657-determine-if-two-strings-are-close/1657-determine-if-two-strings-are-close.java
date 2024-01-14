cc11f5dbd9d31ebf19c16f4e89e61f1167869bc8
Aditya Jadhav <32817064+aditya4196@users.noreply.github.com>
2024-01-14 14:39:24 -0500

    Time: 45 ms (49.27%), Space: 45.7 MB (15.48%) - LeetHub

---
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.java
@@ -0,0 +1,59 @@
+class Solution {
+    public boolean closeStrings(String word1, String word2) {
+        
+        Map<Integer, Integer> countMap1 = new TreeMap();
+        Map<Integer, Integer> countMap2 = new TreeMap();
+        Set<Character> set = new HashSet();
+        
+        int[] freq = new int[26];
+        for(char c : word1.toCharArray()){
+            set.add(c);
+            freq[c-'a']++;
+        }
+        
+        for(int i=0; i<26; i++)countMap1.put(freq[i], countMap1.getOrDefault(freq[i],0)+1);
+        
+        freq = new int[26];
+        for(char c : word2.toCharArray()){
+            if(!set.contains(c)) return false;
+            freq[c-'a']++;
+        }
+        
+        for(int i=0; i<26; i++) countMap2.put(freq[i], countMap2.getOrDefault(freq[i],0)+1);
+        
+        for(int val : countMap1.keySet()){
+            System.out.println(countMap1.get(val) + "-" + countMap2.get(val));
+            if(val == 0) continue;
+            if(countMap1.get(val) != countMap2.get(val)) return false;
+        }
+        
+        return true;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+}
+
+/*
+
+cabbba
+c-1
+b-3
+a-2
+
+abbccc
+a-1
+c-3
+b-2
+
+
+
+*/
