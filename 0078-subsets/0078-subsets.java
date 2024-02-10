978b03ca5b9049b355c33823e239fe4300560cf1
Aditya Jadhav <32817064+aditya4196@users.noreply.github.com>
2024-02-10 15:13:48 -0500

    Time: 1 ms (69.18%), Space: 43.1 MB (8.07%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,17 +1,23 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> result = new ArrayList();
-        backtrack(nums, 0, new ArrayList<Integer>(), result);
-        return result;
-    }
-    
-    public void backtrack(int[] nums, int idx, List<Integer> inter, List<List<Integer>> result){
-        result.add(new ArrayList<Integer>(inter));
+        int n = nums.length;
         
-        for(int i=idx; i<nums.length; i++){
-            inter.add(nums[i]);
-            backtrack(nums, i+1, inter, result);
-            inter.remove(inter.size()-1);
+        for(int i=0; i<(1<<n); i++){
+            List<Integer> inter = new ArrayList();
+            int mask = 1;
+            for(int j=0; j<n; j++){
+                if(((mask<<j)&i) != 0){
+                    inter.add(nums[j]);
+                }
+            }
+            result.add(inter);
         }
+        
+        return result;
+        
+        
+        
+        
     }
 }
