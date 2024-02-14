dd1ff42e2f0a4f73b1af979026bb9b617bcd5a05
Aditya Jadhav <32817064+aditya4196@users.noreply.github.com>
2024-02-14 00:50:21 -0500

    Time: 12 ms (6.15%), Space: 44.5 MB (9.99%) - LeetHub

--- 1408-string-matching-in-an-array/1408-string-matching-in-an-array.java
+++ 1408-string-matching-in-an-array/1408-string-matching-in-an-array.java
@@ -2,11 +2,10 @@ class Solution {
     public List<String> stringMatching(String[] words) {
         
         List<String> result = new ArrayList();
-        Arrays.sort(words, (a,b) -> (a.length() - b.length()));
-        
-        for(int i=0; i<words.length-1; i++){
-            for(int j=i+1; j<words.length; j++){
-                if(isMatched(words[j], words[i])){
+
+        for(int i=0; i<words.length; i++){
+            for(int j=0; j<words.length; j++){
+                if(i!=j && isMatched(words[j], words[i])){
                     result.add(words[i]);
                     break;
                 }
