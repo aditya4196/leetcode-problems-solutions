bcd4bbc8c6ef2e38f3c863dfe39fa67c0d145aa6
Aditya Jadhav <adityajadhav537@gmail.com>
2024-02-18 16:24:02 -0500

    Time: 2 ms (20.97%), Space: 44.3 MB (47.31%) - LeetHub

---
+++ 0768-max-chunks-to-make-sorted-ii/0768-max-chunks-to-make-sorted-ii.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maxChunksToSorted(int[] arr) {
+        int n = arr.length;
+        Deque<Integer> stack = new LinkedList();
+        
+        
+        for(int i=0; i<n; i++){
+            Integer maxVal = -1;
+            while(!stack.isEmpty() && stack.peek() > arr[i]){
+                maxVal = Math.max(maxVal, stack.pop());
+            }
+            if(maxVal != -1){
+                stack.push(maxVal);
+            }
+            else stack.push(arr[i]);
+        }
+        
+        
+        return stack.size();
+       
+        
+    }
+}
