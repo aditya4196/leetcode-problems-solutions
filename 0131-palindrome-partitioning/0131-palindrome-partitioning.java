b22fb211c5ddf7d24251bcda2b7c1a8384557574
Aditya Jadhav <adityajadhav537@gmail.com>
2024-02-19 21:40:21 -0500

    Time: 7 ms (98.66%), Space: 56.9 MB (53.98%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -1,40 +1,24 @@
 class Solution {
     public List<List<String>> partition(String s) {
-        
         List<List<String>> result = new ArrayList();
-        backtrack(s, 0, new ArrayList<String>(), result);
+        backtrack(s, 0, new ArrayList<String>(), result, new boolean[s.length()][s.length()]);
         return result;
         
     }
     
-    public void backtrack(String s, int idx, List<String> inter, List<List<String>> result){
+    public void backtrack(String s, int idx, List<String> inter, List<List<String>> result, boolean[][] dp){
         if(idx == s.length()){
             result.add(new ArrayList<String>(inter));
-            return;
         }
         
         for(int i=idx; i<s.length(); i++){
-            if(checkPalindrome(s, idx, i)){
+            if((s.charAt(idx) == s.charAt(i)) && ((i-idx) <= 2 || dp[idx+1][i-1])){
+                dp[idx][i] = true;
                 inter.add(s.substring(idx, i+1));
-                backtrack(s, i+1, inter, result);
+                backtrack(s, i+1, inter, result, dp);
                 inter.remove(inter.size()-1);
-            }
-        }
-        
-        
-        
-        
-        
-        
-    }
-    
-    public boolean checkPalindrome(String s, int i, int j){
-        while(i < j){
-            if(s.charAt(i) != s.charAt(j)) return false;
-            i++;
-            j--;
-        }
-        return true;
+            }   
+        }   
     }
 }
 
