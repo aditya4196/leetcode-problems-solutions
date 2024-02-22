d56d57c36bebe11a94b32a21ee4329cd21458a83
Aditya Jadhav <adityajadhav537@gmail.com>
2024-02-21 21:48:06 -0500

    Time: 3 ms (71.04%), Space: 54.2 MB (15.73%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.java
+++ 0997-find-the-town-judge/0997-find-the-town-judge.java
@@ -1,18 +1,31 @@
 class Solution {
     public int findJudge(int n, int[][] trust) {
 
-        int[] indegree = new int[n+1];
-        int[] outdegree = new int[n+1];
-
-        for(int[] pair : trust){
-            indegree[pair[1]]++;
-            outdegree[pair[0]]++;
+        if(trust.length < (n-1)) return -1;
+        
+        int[] trustscores = new int[n+1];
+        
+        for(int i=0; i<trust.length; i++){
+            int[] pair = trust[i];
+            trustscores[pair[0]]--;
+            trustscores[pair[1]]++;
         }
-
+        
         for(int i=1; i<=n; i++){
-            if(indegree[i] == (n-1) && outdegree[i] == 0) return i;
-        }
+            if(trustscores[i] == (n-1)) return i;
+         }
         return -1;
         
     }
 }
+
+/*
+
+[1,3][2,3][3,1]
+
+
+boolean[] 
+cand = 3
+
+
+*/
