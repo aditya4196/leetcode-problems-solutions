f15a11ff938daf2b36f32949a97c5a9f030cc08c
Aditya Jadhav <adityajadhav537@gmail.com>
2024-02-25 15:04:02 -0500

    Time: 6 ms (8.03%), Space: 42.1 MB (75.46%) - LeetHub

---
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -0,0 +1,46 @@
+class Solution {
+    int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
+    
+    public int orangesRotting(int[][] grid) {
+        Deque<int[]> queue = new ArrayDeque();
+        int fresh = 0;
+        
+        
+        for(int i=0; i<grid.length; i++){
+            for(int j=0; j<grid[0].length; j++){
+                if(grid[i][j] == 2) queue.offer(new int[]{i,j});
+                else if(grid[i][j] == 1) fresh++;
+            }
+        }
+        
+        if(fresh == 0) return 0;
+        
+        int minutes = -1;
+        
+        while(queue.size() > 0){
+            int size = queue.size();
+            
+            for(int i=1; i<=size; i++){
+                int[] curr = queue.poll();
+                for(int[] dir : dirs){
+                    
+                    int nextx = curr[0] + dir[0];
+                    int nexty = curr[1] + dir[1];
+                    
+                    if(nextx < 0 || nexty < 0 || nextx >= grid.length || nexty >= grid[0].length) continue;
+                    if(grid[nextx][nexty] == 1){
+                        fresh--;
+                        grid[nextx][nexty] = 2;
+                        queue.offer(new int[]{nextx,nexty});
+                    }
+                }
+            }
+            minutes++;
+        }
+        
+        System.out.println(fresh);
+        
+        return (fresh == 0)?minutes:-1;
+        
+    }
+}
