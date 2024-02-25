6321584a380260e3df380581166bc9b3c8e3fe92
Aditya Jadhav <adityajadhav537@gmail.com>
2024-02-25 14:15:34 -0500

    Time: 6 ms (94.48%), Space: 48.7 MB (18.14%) - LeetHub

---
+++ 0286-walls-and-gates/0286-walls-and-gates.java
@@ -0,0 +1,29 @@
+class Solution {
+    final int[][] dirs = {{0,1},{1,0},{0,-1},{-1,0}};
+    
+    public void wallsAndGates(int[][] rooms) {
+        final int m = rooms.length, n = rooms[0].length;
+
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(rooms[i][j] == 0){
+                  dfs(rooms, i, j);  
+                }
+            }
+        }
+    }
+    
+    public void dfs(int[][] rooms, int currx, int curry){
+        for(int[] dir : dirs){
+            int nextx = currx + dir[0];
+            int nexty = curry + dir[1];
+            
+            if(nextx<0 || nexty<0 || nextx>=rooms.length || nexty>=rooms[0].length || rooms[nextx][nexty] == -1) continue;
+            if(rooms[nextx][nexty] > (rooms[currx][curry] + 1)){
+               rooms[nextx][nexty] = rooms[currx][curry] + 1;
+               dfs(rooms, nextx, nexty);
+            }
+        
+        }
+    }
+}
