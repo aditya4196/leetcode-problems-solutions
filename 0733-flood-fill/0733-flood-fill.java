b2cc68b924b3d51dfa522677ac4b321011bf3b15
Aditya Jadhav <adityajadhav537@gmail.com>
2024-02-26 23:04:06 -0500

    Time: 1 ms (44.14%), Space: 45 MB (20.50%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,16 +1,38 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        traverse(image, sr, sc, image[sr][sc], color);
-        return image;
-    }
-    
-    public void traverse(int[][] image, int i, int j, int oldcolor, int newcolor){
-        if(i < 0 || j < 0 || i > image.length-1 || j > image[0].length-1 || image[i][j] == newcolor || image[i][j] != oldcolor) return;
-        image[i][j] = newcolor;
         
-        traverse(image, i-1, j, oldcolor, newcolor);
-        traverse(image, i+1, j, oldcolor, newcolor);
-        traverse(image, i, j-1, oldcolor, newcolor);
-        traverse(image, i, j+1, oldcolor, newcolor);
+        int[][] dirs = {{0,1},{0,-1},{1,0},{-1,0}};
+        
+        int m = image.length, n = image[0].length;
+        if(image[sr][sc] == color) return image;
+        
+        Deque<int[]> queue = new ArrayDeque();
+        int oldColor = image[sr][sc];
+        image[sr][sc] = color;
+        queue.offer(new int[]{sr, sc});
+        
+        while(queue.size() > 0){
+            int size = queue.size();
+            
+            for(int i=1; i<=size; i++){
+                int[] curr = queue.poll();
+                
+                for(int[] dir : dirs){
+                    int nextr = curr[0] + dir[0];
+                    int nextc = curr[1] + dir[1];
+                    
+                    if(nextr < 0 || nextc < 0 || nextr >= m || nextc >= n || image[nextr][nextc] != oldColor) continue;
+                    image[nextr][nextc] = color;
+                    queue.offer(new int[]{nextr, nextc});
+                }
+            }
+        }
+        
+        return image;
+        
+        
+        
+        
+        
     }
 }
