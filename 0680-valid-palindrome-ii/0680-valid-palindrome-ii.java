ed3c156a9ff724a7fa4c7b335f02068c42002e8d
Aditya Jadhav <adityajadhav537@gmail.com>
2024-02-28 17:41:06 -0500

    Time: 14 ms (5.37%), Space: 49.5 MB (5.05%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -1,35 +1,19 @@
 class Solution {
     public boolean validPalindrome(String s) {
-        int n = s.length();
-        return checkPalindrome(s, 0, n-1, 0);
+        return isPalindrome(s, 0, s.length()-1, 0);
     }
     
-    public boolean checkPalindrome(String s, int i, int j, int count){
-        if(count > 1) return false;
+    public boolean isPalindrome(String s, int i, int j, int deleteCount){
+        
+        if(deleteCount == 2) return false;
+        
         if(i>=j) return true;
         
-        if(s.charAt(i) == s.charAt(j)) return checkPalindrome(s, i+1, j-1, count);
+        if(s.charAt(i) == s.charAt(j)){
+            return isPalindrome(s, i+1, j-1, deleteCount);   
+        }
         else{
-            return  checkPalindrome(s, i+1, j, count+1) || checkPalindrome(s, i, j-1, count+1);
+            return isPalindrome(s, i+1, j, deleteCount+1) || isPalindrome(s, i, j-1, deleteCount+1); 
         }
     }
 }
-
-
-/*
-
-
-a b c a
-  i     
-    j
-
-           a b c c d a
-             b c c d
-             
-        c c d       b c c
-        i.  j       i.  j
-      
-           c.    b
-
-
-*/
