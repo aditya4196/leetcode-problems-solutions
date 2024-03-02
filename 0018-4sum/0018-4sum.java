9db5e8b6247869d52a89968e4616d153e2f9faf8
Aditya Jadhav <adityajadhav537@gmail.com>
2024-03-02 10:39:47 -0500

    Time: 19 ms (43.09%), Space: 44.4 MB (40.20%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,5 +1,6 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
+        
         Arrays.sort(nums);
         List<List<Integer>> result = new ArrayList();
         kSum(nums, 0, target, 4, new ArrayList<Integer>(), result);
@@ -7,11 +8,11 @@ class Solution {
         
     }
     
-    public void kSum(int[] nums, int start, long target, int k, List<Integer> quad, List<List<Integer>> result){
-        if(k < 2) return;
-        else if(k == 2){
-            //twosum
-            int left = start, right = nums.length-1;
+    public void kSum(int[] nums, int idx, long target, int k, List<Integer> quad, List<List<Integer>> result){
+        
+        if(k == 2){
+            int left = idx, right = nums.length-1;
+            
             while(left < right){
                 long sum = nums[left] + nums[right];
                 if(sum == target){
@@ -20,22 +21,22 @@ class Solution {
                     result.add(new ArrayList<Integer>(quad));
                     quad.remove(quad.size()-1);
                     quad.remove(quad.size()-1);
-                    left++;
-                    right--;
+                    
+                    left++; right--;
                     while(left < right && nums[left] == nums[left-1]) left++;
                 }
-                else if(sum > target) right--;
-                else left++;
+                else if(sum < target) left++;
+                else right--;
             }
-            
             return;
         }
         
-        for(int i=start; i<nums.length; i++){
-             if(i > start && nums[i] == nums[i-1]) continue;
-             quad.add(nums[i]);
-             kSum(nums, i+1, target-nums[i], k-1, quad, result);
-             quad.remove(quad.size()-1);
+        for(int i=idx; i<=nums.length-k; i++){
+            if(i > idx && nums[i] == nums[i-1]) continue;
+            quad.add(nums[i]);
+            kSum(nums, i+1, target - nums[i], k-1, quad, result);
+            quad.remove(quad.size()-1);
         }
+        
     }
 }
