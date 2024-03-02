70d405e2e5281d2136569e1247cd8368e1003c56
Aditya Jadhav <adityajadhav537@gmail.com>
2024-03-02 17:23:05 -0500

    Time: 103 ms (49.27%), Space: 44.7 MB (30.46%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,20 +1,22 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-        Map<Pair<Integer, Integer>, Integer> cache = new HashMap();
-        return helper(nums, 0, target, cache);
+        
+        Map<Pair<Integer, Integer>, Integer> map = new HashMap();
+        return topDown(nums, target, 0, map);
     }
     
-    public int helper(int[] nums, int idx, int target, Map<Pair<Integer, Integer>, Integer> cache){
-        if(idx >= nums.length && target == 0) return 1;
+    public int topDown(int[] nums, int target, int idx, Map<Pair<Integer,Integer>,Integer> memo){
+        if(idx == nums.length && target == 0) return 1;
         if(idx >= nums.length) return 0;
         
-        if(cache.containsKey(new Pair(idx, target))) return cache.get(new Pair(idx, target));
+        if(memo.containsKey(new Pair(idx, target))) return memo.get(new Pair(idx, target));
         
-        int addOp = helper(nums, idx+1, target + nums[idx], cache);
-        int minOp = helper(nums, idx+1, target - nums[idx], cache);
         
-        int result = addOp + minOp;
-        cache.put(new Pair(idx, target), result);
-        return result;
+        int added = topDown(nums, target - nums[idx], idx+1, memo);
+        int subtracted = topDown(nums, target + nums[idx], idx+1, memo);
+        
+        int total = added + subtracted;
+        memo.put(new Pair(idx, target),total);
+        return total;
     }
 }
