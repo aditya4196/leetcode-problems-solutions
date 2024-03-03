a02ddf8c8f8381159d4e493d5a10dd7c35262579
Aditya Jadhav <adityajadhav537@gmail.com>
2024-03-03 15:32:44 -0500

    Time: 8 ms (65.68%), Space: 56.7 MB (61.01%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -1,21 +1,25 @@
 class Solution {
     
     public List<List<String>> partition(String s) {
-        List<List<String>> result = new ArrayList();     
-        topDown(s, 0, new ArrayList<String>(), result);
+        List<List<String>> result = new ArrayList();
+        boolean[][] dp = new boolean[s.length()][s.length()];
+        
+        
+        topDown(s, 0, new ArrayList<String>(), result, dp);
         return result;
     }
     
-    public void topDown(String s, int idx, List<String> sublist, List<List<String>> result){
+    public void topDown(String s, int idx, List<String> sublist, List<List<String>> result, boolean[][] dp){
         if(idx >= s.length()){
             result.add(new ArrayList<String>(sublist));
             return;
         }
         
         for(int i=idx; i<s.length(); i++){
-            if(isPalindrome(s, idx, i)){
+            if((s.charAt(idx) == s.charAt(i) &&  ((i - idx) <= 2 || dp[idx+1][i-1] == true))){
+                dp[idx][i] = true;
                 sublist.add(s.substring(idx, i+1));
-                topDown(s, i+1, sublist, result);
+                topDown(s, i+1, sublist, result, dp);
                 sublist.remove(sublist.size()-1);
             }
         }
