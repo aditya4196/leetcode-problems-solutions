ade313bddc4c54660c1712b7e207c5d774c2f7b8
Aditya Jadhav <adityajadhav537@gmail.com>
2024-03-03 15:19:55 -0500

    Time: 8 ms (65.68%), Space: 57.1 MB (22.42%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -1,38 +1,44 @@
 class Solution {
+    
     public List<List<String>> partition(String s) {
-        List<List<String>> result = new ArrayList();
-        backtrack(s, 0, new ArrayList<String>(), result, new boolean[s.length()][s.length()]);
+        List<List<String>> result = new ArrayList();     
+        topDown(s, 0, new ArrayList<String>(), result);
         return result;
-        
     }
     
-    public void backtrack(String s, int idx, List<String> inter, List<List<String>> result, boolean[][] dp){
-        if(idx == s.length()){
-            result.add(new ArrayList<String>(inter));
+    public void topDown(String s, int idx, List<String> sublist, List<List<String>> result){
+        if(idx >= s.length()){
+            result.add(new ArrayList<String>(sublist));
+            return;
         }
         
         for(int i=idx; i<s.length(); i++){
-            if((s.charAt(idx) == s.charAt(i)) && ((i-idx) <= 2 || dp[idx+1][i-1])){
-                dp[idx][i] = true;
-                inter.add(s.substring(idx, i+1));
-                backtrack(s, i+1, inter, result, dp);
-                inter.remove(inter.size()-1);
-            }   
-        }   
+            if(isPalindrome(s, idx, i)){
+                sublist.add(s.substring(idx, i+1));
+                topDown(s, i+1, sublist, result);
+                sublist.remove(sublist.size()-1);
+            }
+        }
+        
+        
+    }
+    
+    public boolean isPalindrome(String s, int i, int j){
+        while(i < j){
+            if(s.charAt(i) != s.charAt(j)) return false;
+            i++;j--;
+        }
+        return true;
     }
 }
 
 /*
 
-
-                    aab
+                a a b
                 
-            a,ab
-               
-           a.  a b
-                    
+        a.         a.     b
 
-
+   a a.   a b
 
 
 
