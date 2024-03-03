28b4123a87170d57e3de990cf084faa2aa52fe2b
Aditya Jadhav <adityajadhav537@gmail.com>
2024-03-03 13:44:16 -0500

    Time: 61 ms (27.52%), Space: 65.4 MB (5.31%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
@@ -1,21 +1,31 @@
 class Solution {
     public int longestPalindromeSubseq(String s) {
-        int n = s.length();
-        int[][] dp = new int[n+1][n+1];
         
-        for(int i=1; i<=n; i++){
-            for(int j=1; j<=n; j++){
-                if(s.charAt(i-1) == s.charAt(n-j)){
-                    dp[i][j] = dp[i-1][j-1] + 1;
-                }
-                else{
-                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
-                }
-            }
+        char[] str1 = s.toCharArray();
+        char[] str2 = s.toCharArray(); 
+        reverse(str2);
+        
+        return topDown(str1, str2, str1.length-1, str2.length-1, new Integer[str1.length][str2.length]);
+    }
+    
+    public int topDown(char[] str1, char[] str2, int ptr1, int ptr2, Integer[][] dp){
+        if(ptr1 < 0 || ptr2 < 0) return 0;
+        
+        if(dp[ptr1][ptr2] != null) return dp[ptr1][ptr2];
+        
+        if(str1[ptr1] == str2[ptr2]) return dp[ptr1][ptr2] = 1 + topDown(str1, str2, ptr1-1, ptr2-1, dp);
+        else{
+            return dp[ptr1][ptr2] = Math.max(topDown(str1, str2, ptr1-1, ptr2, dp), topDown(str1, str2, ptr1, ptr2-1, dp));
         }
+    }
+    
+    public void reverse(char[] s){
+        int i = 0, j = s.length-1;
         
-        return dp[n][n];
-        
-        
+        while(i < j){
+            char temp = s[i];
+            s[i++] = s[j];
+            s[j--] = temp;
+        }
     }
 }
