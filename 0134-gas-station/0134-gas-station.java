8ccdef18356bb4e967ff8cbae1dad8538155be61
Aditya Jadhav <adityajadhav537@gmail.com>
2024-03-06 15:26:20 -0500

    Time: 3 ms (47.14%), Space: 56.3 MB (93.18%) - LeetHub

--- 0134-gas-station/0134-gas-station.java
+++ 0134-gas-station/0134-gas-station.java
@@ -1,38 +1,21 @@
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
         
-        int totalGas = 0, start = 0, currGas = 0;
+        int totalGas = 0;
+        int localGas = 0;
+        int start = 0;
         
         for(int i=0; i<gas.length; i++){
-            currGas += gas[i] - cost[i];
+            localGas += gas[i] - cost[i];
             totalGas += gas[i] - cost[i];
             
-            if(currGas < 0){
+            if(localGas < 0){
                 start = i+1;
-                currGas = 0;
+                localGas = 0;
             }
         }
         
         return (totalGas < 0)?-1:start;
         
-        
     }
 }
-
-/*
-
-gas =  [1,2,3,4,5]
-        i
-cost = [3,4,5,1,2]
-        j
-        
-gas - cost = [-2,-2,-2,3,3]
- 
-  0  1  2 3 4
-[-2,-2,-2,3,3]
-
-
-            
-totalTank = -3 
-index = 
-*/
