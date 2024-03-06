391b4a3651000d43cb0d4034a202da1b953459cf
Aditya Jadhav <adityajadhav537@gmail.com>
2024-03-06 16:30:32 -0500

    Time: 69 ms (12.84%), Space: 46.1 MB (14.56%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.java
+++ 0846-hand-of-straights/0846-hand-of-straights.java
@@ -1,33 +1,32 @@
 class Solution {
     public boolean isNStraightHand(int[] hand, int groupSize) {
-        int n = hand.length;
-        if(n%groupSize != 0) return false;
         
-        Map<Integer, Integer> handCount = new TreeMap();
+        if(hand.length % groupSize != 0) return false;
+        
+        Map<Integer, Integer> map = new TreeMap();
         for(int val : hand){
-            handCount.put(val, handCount.getOrDefault(val,0)+1);
+            map.put(val, map.getOrDefault(val,0)+1);
         }
         
-        for(int key : handCount.keySet()){
-            int count = handCount.get(key);
-            int currVal = key;
-            if(count == 0) continue;
+        for(int val : map.keySet()){
+            int currNum = val;
+            int currCount = map.get(val);
+            if(currCount == 0) continue;
             
             for(int i=1; i<=groupSize; i++){
-                if(!handCount.containsKey(currVal) || (handCount.get(currVal) < count)) return false;
-                handCount.put(currVal, handCount.get(currVal)-count);
-                currVal++;
+                if(!map.containsKey(currNum) || map.get(currNum) < currCount) return false;
+                map.put(currNum, map.get(currNum) - currCount);
+                currNum++;
             }
         }
         
         return true;
         
-        
-        
     }
 }
 
 /*
+
 
 1 - 1
 2 - 2
@@ -37,17 +36,12 @@ class Solution {
 7 - 1
 8 - 1
 
-count = 1
 
-
- [1,2,2,3,3,4,6,7,8]
-[                   ]
-condition1 = n is multiple of groupsize
+currCount = 1
+currNum = 1
 
 
 
-n = 9
-gs = 3
 
 
 */
