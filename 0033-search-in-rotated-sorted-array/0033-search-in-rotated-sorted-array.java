86ba830e41b54648b79ce87e9a37e53ee8d80ef0
Aditya Jadhav <adityajadhav537@gmail.com>
2024-03-08 09:52:09 -0500

    Time: 0 ms (100.00%), Space: 41.8 MB (81.63%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,36 +1,37 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int n = nums.length;
-        int startIndex = getStartElementIndex(0, n-1, nums);
         
-        if(target == nums[startIndex]) return startIndex;
-        if(target == nums[n-1]) return n-1;
-
-        if(target > nums[startIndex] && target < nums[n-1]){
-            return binarySearch(startIndex, n-1, nums, target);
-        }
-        else return binarySearch(0, startIndex-1, nums, target);
-
+        int pivot = getPivot(nums, 0, nums.length-1);
+        
+        if(target == nums[pivot]) return pivot;  
+        int result = bsearch(nums, pivot+1, nums.length-1, target);
+        
+        return (result != -1)?result:bsearch(nums, 0, pivot-1, target);
+        
+        
+        
     }
-
-    public int binarySearch(int left, int right, int[] nums, int target){
+    
+    public int getPivot(int[] nums, int left, int right){
+        while(left < right){
+            int pivot = left + (right-left)/2;
+            if(nums[pivot] > nums[right]){
+                left = pivot+1;
+            }
+            else right = pivot;
+        }
+        return left;
+    }
+    
+    public int bsearch(int[] nums, int left, int right, int target){
         while(left <= right){
             int mid = left + (right-left)/2;
             if(nums[mid] == target) return mid;
             else if(nums[mid] > target) right = mid-1;
             else left = mid+1;
         }
+        
         return -1;
-    }
-
-    public int getStartElementIndex(int left, int right, int[] nums){
-        int lastNum = nums[right];
-        while(left < right){
-            int mid = left + (right-left)/2;
-            if(nums[mid] > lastNum) left = mid+1;
-            else right = mid;
-        }
-
-        return left;
+        
     }
 }
