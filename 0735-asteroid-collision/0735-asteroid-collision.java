129991e3cd81e06b9a326a5b395f8cfd9ea883fc
Aditya Jadhav <adityajadhav537@gmail.com>
2024-03-09 14:16:40 -0500

    Time: 6 ms (61.50%), Space: 45 MB (91.37%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.java
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -1,37 +1,60 @@
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
-        Deque<Integer> stack = new LinkedList();
         
-        int idx = 0;
-        while(idx < asteroids.length){
-            int curr = asteroids[idx];
-            if(curr < 0 && !stack.isEmpty() && stack.peek() > 0){
-                    if(stack.peek() > -1*curr){
+        int idx=0, n = asteroids.length;
+        Stack<Integer> stack = new Stack<Integer>();
+        
+        while(idx < n){
+            //if(!stack.isEmpty()) System.out.println(stack.peek());
+            int currAsteroid = asteroids[idx];
+            if(currAsteroid < 0 && !stack.isEmpty() && stack.peek() > 0){
+                    int diff = stack.peek() - (-1*currAsteroid);
+                    //System.out.println(diff);
+                    if(diff == 0){
+                        stack.pop();
                         idx++;
                     }
-                    else if(stack.peek() < -1*curr){
-                        stack.pop();
-                    }
+                    else if(diff < 0) stack.pop();
                     else{
                         idx++;
-                        stack.pop();
-                    }
-                }
-                else{
-                    stack.push(curr);
-                    idx++;
-                }
+                    } 
+            }
+            else{
+             stack.push(currAsteroid);
+             idx++;
+            }
         }
         
         int[] result = new int[stack.size()];
-        
-        int i=stack.size()-1;
-        while(!stack.isEmpty()){
-            result[i--] = stack.pop();
+        int i=0;
+        for(int asteroid : stack){
+            result[i++] = asteroid;
         }
         
         return result;
-        
-        
     }
 }
+
+/*
+
+
+[-2,2,-1,-2]
+       i
+ 
+ 
+ stack = [-2,2,]
+
+if(asteroid < 0){
+    if(!stack.isEmpty() && stack.peek() > 0){
+        int diff = stack.peek() - (-1*asteroid){
+        
+        if(diff == 0){
+          stack.pop(); index++;
+        }
+        }
+    }
+}
+
+
+
+*/
