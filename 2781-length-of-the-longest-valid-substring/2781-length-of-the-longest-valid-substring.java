c800dc34809c08b4c94abc5e08a91fe36205663a
Aditya Jadhav <adityajadhav537@gmail.com>
2024-03-09 22:57:05 -0500

    Time: 138 ms (69.48%), Space: 61.6 MB (92.21%) - LeetHub

--- 2781-length-of-the-longest-valid-substring/2781-length-of-the-longest-valid-substring.java
+++ 2781-length-of-the-longest-valid-substring/2781-length-of-the-longest-valid-substring.java
@@ -1,36 +1,23 @@
 class Solution {
     public int longestValidSubstring(String word, List<String> forbidden) {
-        Set<String> set = new HashSet<>(forbidden);
-        int i = 0, j = 0, max = 0, n = word.length();
+        
+        HashSet<String> forbiddenSet = new HashSet<String>(forbidden);
+        int maxLength = 0, n = word.length();
         int end = n-1;
         
-        for(i = n-1; i >= 0; i--) {
-            for(j = i; j <= end && j < i + 10; j++) {
-                String sub = word.substring(i, j + 1);
-                // System.out.println("i = " + i + " j = " + j + " sub = " + sub + " end = " + end);
-                if(set.contains(sub)) {
-                    end = j - 1;
+        for(int i=n-1; i>=0; i--){
+            for(int j=i; (j<=end && (j-i) < 10); j++){
+                String checkString = word.substring(i, j+1);
+                if(forbiddenSet.contains(checkString)){
+                    end = j-1;
                     break;
                 }
             }
-            max = Math.max(max, end-i+1);
+            
+            maxLength = Math.max(maxLength, end-i+1);
         }
         
-        return max;
+        return maxLength;
+        
     }
 }
-
-/*
-
-leetcode
-       i
-       j
-
-end = n-1
-
-["de","le","e"]
-  
-set(de,le,e)
-
-
-*/
