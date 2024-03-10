d0699b7cab5e927e01c997032cf12fc42aa93c30
Aditya Jadhav <adityajadhav537@gmail.com>
2024-03-10 11:37:39 -0400

    Time: 0 ms (100.00%), Space: 46.4 MB (35.68%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,31 +1,24 @@
 class Solution {
     public int trap(int[] height) {
         
-        //container with most water concept
         int leftMax = 0, rightMax = 0;
-        int left = 0, right = height.length-1;
-        int waterTrapped = 0;
+        int left = 0, right = height.length-1, waterTrapped = 0;
         
         while(left < right){
             if(height[left] > height[right]){
-                if(rightMax <= height[right]){
-                    rightMax = height[right];
-                }
+                if(rightMax <= height[right]) rightMax = height[right];
                 else waterTrapped += rightMax - height[right];
                 right--;
             }
             else{
-                if(leftMax <= height[left]){
-                    leftMax = height[left];
-                }
+                if(leftMax <= height[left]) leftMax = height[left];
                 else waterTrapped += leftMax - height[left];
                 left++;
             }
+            
         }
         
         return waterTrapped;
-        
-        
         
         
         
@@ -34,11 +27,14 @@ class Solution {
 
 /*
 
-O(n)
-[0,1,0,2,1,0,1,3,2,1,2,1]
-[0,1,1,2,2,2,2,3,3,3,3,3]
-[3,3,3,3,3,3,3,3,2,2,2,1]
+0,1,0,2,1,0,1,3,2,1,2,1
+              i
+              j
+  
+watertrapped = 6
+leftmax = 2
+rightmax = 2
 
 
-right = 1
+
 */
