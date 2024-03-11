8c8b43b03a109ce09d3012eeb1d7e3fc8c3e0bf7
Aditya Jadhav <adityajadhav537@gmail.com>
2024-03-11 18:04:07 -0400

    Time: 139 ms (65.79%), Space: 62.3 MB (55.55%) - LeetHub

--- 2781-length-of-the-longest-valid-substring/2781-length-of-the-longest-valid-substring.java
+++ 2781-length-of-the-longest-valid-substring/2781-length-of-the-longest-valid-substring.java
@@ -1,23 +1,41 @@
 class Solution {
     public int longestValidSubstring(String word, List<String> forbidden) {
         
+        int n = word.length();
         HashSet<String> forbiddenSet = new HashSet<String>(forbidden);
-        int maxLength = 0, n = word.length();
         int end = n-1;
+        int maxLength = 0;
+        int i=0, j=0;
         
-        for(int i=n-1; i>=0; i--){
-            for(int j=i; (j<=end && (j-i) <= 10); j++){
-                String checkString = word.substring(i, j+1);
-                if(forbiddenSet.contains(checkString)){
+        for(i=n-1; i>=0; i--){
+            for(j=i; ((j-i) < 10) && (j<=end); j++){
+                String toCheck = word.substring(i, j+1);
+                if(forbiddenSet.contains(toCheck)){
                     end = j-1;
                     break;
                 }
             }
             
             maxLength = Math.max(maxLength, end-i+1);
+            
+            
+            
         }
         
         return maxLength;
         
+        
     }
 }
+
+
+/*
+
+cbaaaabc
+ i
+ j
+   e
+
+3
+
+*/
