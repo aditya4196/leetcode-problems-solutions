9c2b36b027a1223f91e185256493aeef501158ec
Aditya Jadhav <adityajadhav537@gmail.com>
2024-03-15 01:10:23 -0400

    Time: 6 ms (90.98%), Space: 45.7 MB (6.02%) - LeetHub

---
+++ 0720-longest-word-in-dictionary/0720-longest-word-in-dictionary.java
@@ -0,0 +1,81 @@
+class Solution {
+    
+    TrieNode root;
+    String maxString;
+    int maxLength;
+    
+    public Solution(){
+        this.root = new TrieNode();
+        maxString = "";
+        maxLength = 0;
+    }
+    
+    
+    public String longestWord(String[] words) {
+        for(String word : words){
+            addWord(word);
+        }
+
+        for(int i=0; i<26; i++){
+            traverse(root.children[i], 0);
+        }
+        return maxString;
+    }
+    
+    public void traverse(TrieNode curr, int prevCount){
+        if(curr == null) return;
+        
+        if(!curr.isWord) return;
+        if(curr.count == (prevCount+1)){
+            if(maxLength < curr.count){
+                maxLength = curr.count;
+                maxString = curr.word;
+            }
+        }
+        
+        for(int i=0; i<26; i++){
+            traverse(curr.children[i], curr.count);
+        }
+    }
+    
+    
+    
+    public void addWord(String word){
+        TrieNode curr = root;
+        for(int i=0; i<word.length(); i++){
+            char c = word.charAt(i);
+            if(curr.children[c-'a'] == null) curr.children[c-'a'] = new TrieNode();
+            curr = curr.children[c-'a'];
+        }
+        curr.isWord = true;
+        curr.word = word;
+        curr.count = word.length();
+    }
+}       
+
+class TrieNode{
+    public TrieNode[] children;
+    public boolean isWord;
+    public int count;
+    public String word;
+    
+    public TrieNode(){
+        this.children = new TrieNode[26];
+        this.isWord = false;
+        this.count = 0;
+        this.word = new String();
+    }
+}
+
+/*
+
+                        *
+                        w(t)
+                        o(t)
+                        r(t)
+                        l(t)
+                        d(t)
+
+
+
+*/
