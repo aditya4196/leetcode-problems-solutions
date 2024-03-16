a70e351e3b6f1e706333a1996369ac1c6a59bc25
Aditya Jadhav <adityajadhav537@gmail.com>
2024-03-16 14:22:11 -0400

    Time: 1567 ms (5.02%), Space: 80.6 MB (30.50%) - LeetHub

--- 0827-making-a-large-island/0827-making-a-large-island.java
+++ 0827-making-a-large-island/0827-making-a-large-island.java
@@ -1,98 +1,125 @@
 class Solution {
-    
-    private int maxArea;
-    
-    private int[][] dirs = {{0,1},{1,0},{-1,0},{0,-1}};
-    
+    int[][] dirs = {{0,1},{0,-1},{1,0},{-1,0}};
     public int largestIsland(int[][] grid) {
+        //dfs to form islands and the dsu instance
+        int n = grid.length;
         
-        initMaxArea();
-        Map<Integer, Integer> islandMap = new HashMap();
-        int m = grid.length, n = grid[0].length;
-
-        int[][] components = new int[m][n];
+        DSU dsu = new DSU(n);
+        boolean[][] visited = new boolean[n][n];
+        boolean hasZeros = false;
         
-        int islandId = 2;
-        boolean hasZero = false;
-        
-        
-        //dfs to get islands
-        for(int i=0; i<m; i++){
+        for(int i=0; i<n; i++){
             for(int j=0; j<n; j++){
-                if(grid[i][j] == 1 && components[i][j] == 0){
-                    setMaxArea(dfs(grid, i, j, islandId, components));
-                    islandMap.put(islandId, getMaxArea());
-                    initMaxArea();
-                    islandId++;
+                if(grid[i][j] == 1 && !visited[i][j]){
+                    dfs(grid, i, j, visited, dsu);
                 }
-                else if(grid[i][j] == 0) hasZero = true;
+                if(grid[i][j] == 0) hasZeros = true;
             }
         }
         
-        if(!hasZero) return m*n;
+        if(!hasZeros) return n*n;
         
-        int largeArea = 0;
+        // for(int i=0; i<n*n; i++){
+        //     System.out.println(i + "-" + dsu.parent[i] + "-" + dsu.size[i]);
+        // }
         
-        //go through zeros and combine the islands
-        int[][] dirs = {{0,1},{1,0},{0,-1},{-1,0}};
-
-
+        int maxArea = 0;
         
-        Set<Integer> addedIsland = new HashSet();
-        
-        for(int i=0; i<m; i++){
+        for(int i=0; i<n; i++){
             for(int j=0; j<n; j++){
-                addedIsland = new HashSet();
-                int addedArea = 1;
-                if(components[i][j] == 0){
+                
+                if(grid[i][j] == 0){
+                    int count = 1;
+                    Set<Integer> set = new HashSet();
+                    
                     for(int[] dir : dirs){
-                        int ni = i + dir[0];
-                        int nj = j + dir[1];
-                        
-                        if(isBoundaryValid(ni, nj, m, n) && components[ni][nj] != 0 && !addedIsland.contains(components[ni][nj])){
-                            addedArea += islandMap.get(components[ni][nj]);
-                            addedIsland.add(components[ni][nj]);
+                        int adj_i = i + dir[0];
+                        int adj_j = j + dir[1];
+                        int adj_idx = adj_i*n + adj_j;
+                        if(isValidBounds(adj_i, adj_j, n) 
+                           && grid[adj_i][adj_j] == 1 && !set.contains(dsu.parent[adj_idx])){
+                            count += dsu.size[dsu.parent[adj_idx]];
+                            set.add(dsu.parent[adj_idx]);
                         }
-                    }
-                    largeArea = Math.max(largeArea, addedArea);
-                }
+                     }
+                    System.out.println(count);
+                    maxArea = Math.max(maxArea, count);
+                }   
             }
         }
         
-        return largeArea;
-    }
-    
-    public int dfs(int[][] grid, int i, int j, int isid, int[][] components){
-        if(!isBoundaryValid(i,j,grid.length, grid[0].length) || grid[i][j] == 0 || components[i][j] != 0) return 0;
-        
-        components[i][j] = isid;
-        
-        int count = 1;
-        for(int[] dir : dirs){
-            int ni = i + dir[0];
-            int nj = j + dir[1];
-            count += dfs(grid, ni, nj, isid, components);
-        }
-        
-        return count;
-    }
-    
-    public boolean isBoundaryValid(int i, int j, int m, int n){
-        return (i>=0 && j>=0 && i<m && j<n);
-    }
-    
-    
-    
-    public void initMaxArea(){
-        maxArea = 0;
-    }
-    
-    public void setMaxArea(int newArea){
-        maxArea = Math.max(maxArea, newArea);
-    }
-    
-    public int getMaxArea(){
         return maxArea;
+        
+        
+        
     }
-
+    
+    public boolean isValidBounds(int i, int j, int n){
+        return (i>=0 && j>=0 && i<n && j<n);
+    }
+    
+    public void dfs(int[][] grid, int i, int j, boolean[][] visited, DSU dsu){
+        visited[i][j] = true;
+        int idx = i*grid.length + j;
+        
+        for(int[] dir : dirs){
+            int adj_i = i + dir[0];
+            int adj_j = j + dir[1];
+            int adj_idx = adj_i*grid.length + adj_j;
+            
+            if(isValidBounds(adj_i, adj_j, grid.length) && !visited[adj_i][adj_j] && grid[adj_i][adj_j] == 1){
+                dsu.unionBySize(idx, adj_idx);
+                dfs(grid, adj_i, adj_j, visited, dsu);
+            }
+        }
+    }
 }
+
+class DSU{
+    int[] parent;
+    int[] size;
+    
+    public DSU(int n){
+        this.parent = new int[n*n+1];
+        this.size = new int[n*n+1];
+        for(int i=0; i<n*n; i++){
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+    
+    public int findParent(int node){
+        if(node == parent[node]) return node;
+        else return node = findParent(parent[node]);
+    }
+    
+    public void unionBySize(int node1, int node2){
+        int fnode1 = findParent(node1);
+        int fnode2 = findParent(node2);
+        
+        if(fnode1 == fnode2) return;
+        
+        if(size[fnode1] >= size[fnode2]){
+            parent[fnode2] = fnode1;
+            size[fnode1] += size[fnode2];
+        }
+        else if(size[fnode2] > size[fnode1]){
+            parent[fnode1] = fnode2;
+            size[fnode2] += size[fnode1];
+        }
+    }
+    
+}
+
+/*
+
+[1,1,0]
+[0,0,1]
+[1,0,1]
+
+
+idx = i*n+j
+i,j = idx/n,id%n
+
+
+*/
