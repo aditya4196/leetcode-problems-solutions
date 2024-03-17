28470d67aa1149ee0e48c088bf34e1a4f6c1b49f
Aditya Jadhav <adityajadhav537@gmail.com>
2024-03-17 13:10:31 -0400

    Time: 7 ms (18.11%), Space: 41.4 MB (81.89%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.java
+++ 0767-reorganize-string/0767-reorganize-string.java
@@ -1,33 +1,34 @@
 class Solution {
     public String reorganizeString(String s) {
         
+        if(s.length() == 0 || s.length() == 1) return s;
+        
         HashMap<Character, Integer> countMap = new HashMap();
         for(char c : s.toCharArray()){
             countMap.put(c, countMap.getOrDefault(c,0)+1);
         }
         
         PriorityQueue<Character> pq = new PriorityQueue<Character>((a,b) -> (countMap.get(b) - countMap.get(a)));
-        
-        for(char c : countMap.keySet()){
-            pq.offer(c);
-        }
+        for(char c : countMap.keySet()) pq.offer(c);
         
         StringBuilder result = new StringBuilder();
+        
         while(pq.size() > 0){
             char first = pq.poll();
             
-            if(result.length() == 0 || (first != result.charAt(result.length()-1))){
+            if(result.length() == 0 || (result.charAt(result.length()-1) != first)){
                 result.append(first);
                 countMap.put(first, countMap.get(first)-1);
                 if(countMap.get(first) > 0) pq.offer(first);
             }
+            
             else{
                 if(pq.size() == 0) return "";
-
                 char second = pq.poll();
                 result.append(second);
                 countMap.put(second, countMap.get(second)-1);
                 if(countMap.get(second) > 0) pq.offer(second);
+                
                 pq.offer(first);
             }
         }
@@ -35,23 +36,7 @@ class Solution {
         return result.toString();
         
         
+        
+        
     }
 }
-
-/*
-
-
-a - 2
-b - 2
-c - 1
-
-pq = [a,b,c]
-polled = a
-
-result = a
-
-    
-
-
-
-*/
