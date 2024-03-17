0c96ff1e8f272d37f15e213c22c0fdeb82a5b9fb
Aditya Jadhav <adityajadhav537@gmail.com>
2024-03-17 10:24:48 -0400

    Time: 4 ms (99.29%), Space: 55.7 MB (14.47%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
@@ -1,46 +1,33 @@
 class Solution {
-    boolean[] cyclicNode;
     public List<Integer> eventualSafeNodes(int[][] graph) {
+        
         int n = graph.length;
+        boolean[] visited = new boolean[n];
+        boolean[] isCyclic = new boolean[n];
+        
+        for(int i=0; i<n; i++){
+            dfs(graph, i, visited, isCyclic);
+        }
         List<Integer> result = new ArrayList();
-        cyclicNode = new boolean[n];
-        
         for(int i=0; i<n; i++){
-            isCycle(graph, i, new boolean[n], cyclicNode);
+            if(!isCyclic[i]) result.add(i);
         }
-        
-        for(int i=0; i<n; i++){
-            if(!cyclicNode[i]) result.add(i);
-        }
-        
         return result;
+        
     }
     
-    public boolean isCycle(int[][] graph, int curr, boolean[] visited, boolean[] cyclicNode){
-        if(cyclicNode[curr]) return true;
+    public boolean dfs(int[][] graph, int curr, boolean[] visited, boolean[] isCyclic){
+        if(isCyclic[curr]) return true;
         if(visited[curr]) return false;
         
-        cyclicNode[curr] = true;
+        isCyclic[curr] = true;
         visited[curr] = true;
         
-        for(int adj : graph[curr]){
-            if(isCycle(graph, adj, visited, cyclicNode)) return true;
+        for(int next : graph[curr]){
+            if(dfs(graph, next, visited, isCyclic)) return true;
         }
         
-        cyclicNode[curr] = false;
+        isCyclic[curr] = false;
         return false;
     }
-
 }
-
-
-/*
-
-5 -> (2,4)
-(2,4) -> {
-    2 -> 
-    4 -> (since it is empty)
-}
-
-
-*/
