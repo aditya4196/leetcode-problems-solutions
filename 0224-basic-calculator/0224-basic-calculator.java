c7b931bfdfcca08473f407415ae08b87fc93f81d
Aditya Jadhav <adityajadhav537@gmail.com>
2024-03-18 17:54:04 -0400

    Time: 197 ms (5.01%), Space: 46.2 MB (14.10%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.java
+++ 0224-basic-calculator/0224-basic-calculator.java
@@ -1,24 +1,24 @@
 class Solution {
     public int calculate(String s) {
-        
-        int num = 0, result = 0,sign = 1;
         Stack<Integer> stack = new Stack<Integer>();
+        int result = 0, num = 0, sign = 1;
         
         for(int i=0; i<s.length(); i++){
             char c = s.charAt(i);
             
             if(Character.isDigit(c)){
                 num = num*10 + (c-'0');
+                System.out.println(num);
             }
             else if(c == '+'){
-                result += (num*sign);
-                sign = 1;
+                result += num*sign;
                 num = 0;
+                sign = 1;
             }
             else if(c == '-'){
-                result += (num*sign);
-                sign = -1;
+                result += num*sign;
                 num = 0;
+                sign = -1;
             }
             else if(c == '('){
                 stack.push(result);
@@ -28,13 +28,20 @@ class Solution {
                 sign = 1;
             }
             else if(c == ')'){
+                
                 result += num*sign;
                 num = 0;
-                result *= stack.pop();
-                result += stack.pop();
+                result = result*stack.pop();
+                result = result + stack.pop();
             }
         }
+        result += num*sign;
         
-        return result + (num*sign);
+        return result;
+        
+        
+        
     }
+    
+    
 }
