aeb2691f3e9276adec3b3297f6dc98c05b77545f
Aditya Jadhav <adityajadhav537@gmail.com>
2024-03-19 19:06:56 -0400

    Time: 1 ms (98.58%), Space: 44.7 MB (54.82%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -10,12 +10,11 @@ class Solution {
             i++;
         }
         
-        while(i<n && (newInterval[0] <= intervals[i][1] && newInterval[1] >= intervals[i][0])){
+        while(i<n && (newInterval[1] >= intervals[i][0])){
             newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
             newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
             i++;
         }
-        
         result.add(newInterval);
         
         while(i < n){
