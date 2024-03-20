34a2a821d54c86e87b33cddae782c591c85564bf
Aditya Jadhav <adityajadhav537@gmail.com>
2024-03-20 14:39:16 -0400

    Time: 9 ms (13.54%), Space: 57.3 MB (88.49%) - LeetHub

--- 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.java
+++ 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.java
@@ -1,15 +1,16 @@
 class Solution {
     public int maximumElementAfterDecrementingAndRearranging(int[] arr) {
-        
         Arrays.sort(arr);
+        int maxNum = 1;
         arr[0] = 1;
         for(int i=1; i<arr.length; i++){
-            if(Math.abs(arr[i] - arr[i-1]) > 1){
-                arr[i] = arr[i-1]+1;
-            }
+            int diff = Math.abs(arr[i]-arr[i-1]);
+            if(diff > 1) arr[i] = arr[i-1] + 1;
+            maxNum = Math.max(maxNum, arr[i]);
         }
         
-        return arr[arr.length-1];
+        return maxNum;
+        
         
     }
 }
@@ -17,15 +18,18 @@ class Solution {
 
 /*
 
- 0.  1.  2
+[5,11,14,3,2]
+[2,3,5,11,14]
+
+[1,2,3,4,5]
+
+
+
+
 [100,1,1000]
-  
-[1,100]
 
+[1,100,1000]
 
-1 - 0
-100 - 1
-1000 - 1
 
 
 */
