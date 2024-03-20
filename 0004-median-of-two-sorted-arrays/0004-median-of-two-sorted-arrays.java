56560231eb01d84119efa30b4df48c4a040930dd
Aditya Jadhav <adityajadhav537@gmail.com>
2024-03-20 13:33:59 -0400

    Time: 1 ms (100.00%), Space: 45.6 MB (88.33%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,43 +1,66 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+         
+        int x = nums1.length, y = nums2.length;
+        if(x > y) return findMedianSortedArrays(nums2, nums1);
         
-        if(nums1.length > nums2.length) return findMedianSortedArrays(nums2, nums1);
-        
-        int x = nums1.length;
-        int y = nums2.length;
-        
-        boolean isEvenLength = (x+y)%2 == 0;
-        
-        int maxLeftX = 0, minRightX = 0, maxLeftY = 0, minRightY = 0;
+        boolean isEvenLength = ((x+y)%2 == 0);
         int left = 0, right = x;
         
         while(left <= right){
-            int partitionX = left + (right-left)/2;
-            int partitionY = (x+y+1)/2 - partitionX;
+            int partX = left + (right-left)/2;
+            int partY = (x + y + 1)/2 - partX;
             
-            maxLeftX = (partitionX == 0)?(Integer.MIN_VALUE):(nums1[partitionX-1]);
-            maxLeftY = (partitionY == 0)?(Integer.MIN_VALUE):(nums2[partitionY-1]);
-            minRightX = (partitionX == x)?(Integer.MAX_VALUE):(nums1[partitionX]);
-            minRightY = (partitionY == y)?(Integer.MAX_VALUE):(nums2[partitionY]);
+            int minLeftX = (partX == 0)?(Integer.MIN_VALUE):(nums1[partX-1]);
+            int minLeftY = (partY == 0)?(Integer.MIN_VALUE):(nums2[partY-1]);
+            int maxRightX = (partX == x)?(Integer.MAX_VALUE):(nums1[partX]);
+            int maxRightY = (partY == y)?(Integer.MAX_VALUE):(nums2[partY]);
             
-            if(maxLeftX <= minRightY && maxLeftY <= minRightX){
-                if(isEvenLength){
-                    return ((double)Math.max(maxLeftX, maxLeftY) + (double)Math.min(minRightX, minRightY)) / 2;
-                }
-                else{
-                    return Math.max(maxLeftX, maxLeftY);
-                }
+            if(minLeftX <= maxRightY && minLeftY <= maxRightX){
+                if(isEvenLength) return (double)(Math.min(maxRightX, maxRightY) + Math.max(minLeftX, minLeftY))/2;
+                else return (double)(Math.max(minLeftX, minLeftY));
             }
-            else if(maxLeftX > minRightY){
-                right = partitionX-1;
+            
+            if(minLeftX > maxRightY){
+                right = partX-1;
             }
-            else{
-                left = partitionX+1;
+            else if(minLeftY > maxRightX){
+                left = partX+1;
             }
         }
         
         return -1.0;
-        
-        
     }
 }
+
+/*
+
+[1,3]
+[2,4,5]
+ 0 1
+[1,3] y = 2
+ m
+ 
+ left + (right-left)/2
+ 
+ partitiony = 1
+ 
+ partitionx = (3 - 1) = 2
+ 
+
+[2,4,5] x = 3
+
+total = 5
+
+[3]
+ x
+   
+[2,4,5]
+       y
+     
+[-1,0 |6]
+[1  | 2]
+
+
+
+*/
