cef4f7d37c88e57f9b10c6da225aebacf0a60b6e
Aditya Jadhav <adityajadhav537@gmail.com>
2024-03-20 22:50:42 -0400

    Time: 621 ms (18.74%), Space: 56.3 MB (6.64%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.java
+++ 0127-word-ladder/0127-word-ladder.java
@@ -1,96 +1,56 @@
 class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
         
-        HashSet<String> wordSet = new HashSet<String>(wordList);
-        HashMap<String, LinkedList<String>> adjlist = new HashMap();
-        
-        
-        StringBuilder wordBuilder = new StringBuilder(beginWord);
-        for(int i=0; i<beginWord.length(); i++){
-                for(char c = 'a'; c <= 'z'; c++){
-                    StringBuilder temp = new StringBuilder(wordBuilder);
-                    temp.setCharAt(i,c);
-                    if(temp.toString().equals(beginWord)) continue;
-                    if(wordSet.contains(temp.toString())){
+        Map<String, List<String>> adjlist = new HashMap();
+        Set<String> wordSet = new HashSet<String>(wordList);
 
-                        adjlist.computeIfAbsent(beginWord, k -> new LinkedList<String>()).add(temp.toString());
-                    }
-                }     
-        }
-        
         for(String word : wordList){
-            wordBuilder = new StringBuilder(word);
-            
             for(int i=0; i<word.length(); i++){
-                
-                for(char c = 'a'; c <= 'z'; c++){
-                    StringBuilder temp = new StringBuilder(wordBuilder);
-                    temp.setCharAt(i,c);
-                    if(temp.toString().equals(word)) continue;
-                    if(wordSet.contains(temp.toString())){
-                        adjlist.computeIfAbsent(word, k -> new LinkedList<String>()).add(temp.toString());
+                for(char c = 'a'; c<='z'; c++){
+                    String newWord = word.substring(0,i) + String.valueOf(c) + word.substring(i+1);
+                    if(wordSet.contains(newWord)){
+                        adjlist.computeIfAbsent(word, k -> new ArrayList<String>()).add(newWord);
                     }
                 }
-                
             }
-           
         }
-        
-        // for(String key : adjlist.keySet()){
-        //     System.out.println(key);
-        //     System.out.println(Arrays.asList(adjlist.get(key)));
-        //     System.out.println();
-        // }
-        
-        return bfs(adjlist, beginWord, endWord);        
-        
-    }
-    
-    public int bfs(HashMap<String, LinkedList<String>> adjlist, String beginWord, String endWord){
-        int count = 0;
-        
-        Deque<String> queue = new ArrayDeque();
+
+        //handle begin word
+        for(int i=0; i<beginWord.length(); i++){
+            for(char c = 'a'; c<='z'; c++){
+                String newWord = beginWord.substring(0,i) + String.valueOf(c) + beginWord.substring(i+1);
+                if(wordSet.contains(newWord)){
+                    adjlist.computeIfAbsent(beginWord, k-> new ArrayList<String>()).add(newWord);
+                }
+            }
+        }
+
+        //bfs over adjlist
+        Deque<String> queue = new LinkedList();
+        int dist = 1;
         queue.offer(beginWord);
-        HashSet<String> visited = new HashSet();
-        
+        Set<String> visited = new HashSet<String>();
+
         while(queue.size() > 0){
             int size = queue.size();
-            count++;
 
             for(int i=1; i<=size; i++){
                 String curr = queue.poll();
+                if(curr.equals(endWord)) return dist;
                 visited.add(curr);
 
-                if(curr.equals(endWord)) return count;
-                
-                if(!adjlist.containsKey(curr)) continue;
-                
-                for(String nextword : adjlist.get(curr)){
-                    if(!visited.contains(nextword)){
-                        queue.offer(nextword);
-                    }    
+                for(String nextWord : adjlist.get(curr)){
+                    if(!visited.contains(nextWord)){
+                        queue.offer(nextWord);
+                    }
                 }
             }
+            dist++;
         }
-        
+
         return 0;
-        
+
+
         
     }
 }
-
-/*
-
-["hot","dot","dog","lot","log","cog"]
-
-bword = hit
-eword = cog
-  
-                                  *
-                        h     d      l      c
-                       o      o      o      o
-                      t     t   g  t   g    g
-                      
-
-     
-*/
