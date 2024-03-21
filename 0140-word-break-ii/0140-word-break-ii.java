0f96c19ff5df8e524cca88b9822746d90fb30967
Aditya Jadhav <adityajadhav537@gmail.com>
2024-03-21 00:09:05 -0400

    Time: 2 ms (58.03%), Space: 41.5 MB (71.15%) - LeetHub

---
+++ 0140-word-break-ii/0140-word-break-ii.java
@@ -0,0 +1,56 @@
+class Solution {
+    List<String> result = new ArrayList();
+    Set<String> wordSet;
+    public List<String> wordBreak(String s, List<String> wordDict) {
+        wordSet = new HashSet<String>(wordDict);
+        helper(s, 0, new String());    
+        return result;
+    }
+    
+    public void helper(String s, int idx, String suboutput){
+        if(idx >= s.length()){
+            System.out.println(suboutput);
+            if(suboutput.length()>0){
+                result.add(new String(suboutput));
+            }
+            return;
+        }
+        
+        for(int i=idx; i<s.length(); i++){
+            String subword = s.substring(idx,i+1);
+            if(wordSet.contains(subword)){
+                helper(s, i+1, suboutput + ((suboutput.length() == 0)?subword:" "+subword));
+            }
+        }
+    }
+}
+
+
+
+/*
+["cat","cats","and","sand","dog"]
+
+                                catsanddog
+ 
+                                    ()
+                                    
+    (c) (ca) (cat)           ()(cats)     (catsa)      (catsan)      (catsand)
+                           
+                   (cats )(a)   (cats )(and) 
+          
+                                    (cats and )()
+                            
+                        
+
+
+
+
+
+
+
+
+
+
+
+
+*/
