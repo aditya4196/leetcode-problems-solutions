8e919da9fe1562d969884bd7e9f2c8d6d2c10c05
Aditya Jadhav <adityajadhav537@gmail.com>
2024-03-23 23:21:14 -0400

    Time: 5 ms (94.96%), Space: 48.8 MB (75.36%) - LeetHub

--- 0978-longest-turbulent-subarray/0978-longest-turbulent-subarray.java
+++ 0978-longest-turbulent-subarray/0978-longest-turbulent-subarray.java
@@ -1,40 +1,31 @@
 class Solution {
     public int maxTurbulenceSize(int[] arr) {
         
-        
-        //> : 1, < : -1, = : 0
+        int right = 1, left = 0;
+        int maxlen = 1;
         int sign = 0;
-        int maxLength = 1, left = 0, right = 1;
         
         while(right < arr.length){
             if(arr[right-1] > arr[right] && sign != 1){
-                maxLength = Math.max(maxLength, right-left+1);
+                maxlen = Math.max(maxlen, right-left+1);
                 right++;
                 sign = 1;
             }
             else if(arr[right-1] < arr[right] && sign != -1){
-                maxLength = Math.max(maxLength, right-left+1);
+                maxlen = Math.max(maxlen, right-left+1);
                 right++;
                 sign = -1;
             }
             else{
-                if(arr[right-1] == arr[right]) right++;
+                if(arr[right-1] == arr[right]) right+=1;
                 left = right-1;
                 sign = 0;
             }
         }
         
-        return maxLength;
+        return maxlen;
+        
+        
         
     }
 }
-
-/*
-
-[9,4,2,10,7,8,8,1,9]
- e o e o  e o e o e
-   i
- 
- evenFlip = true;
- oddFlip = false;
-*/
