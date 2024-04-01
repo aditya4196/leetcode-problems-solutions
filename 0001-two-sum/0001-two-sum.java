cad2f25f55688d39351897cd1368cd2f5c8e7474
Aditya Jadhav <adityajadhav537@gmail.com>
2024-04-01 10:17:47 -0400

    Time: 2 ms (97.83%), Space: 44.5 MB (90.87%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -4,10 +4,11 @@ class Solution {
         Map<Integer, Integer> map = new HashMap();
         
         for(int i=0; i<nums.length; i++){
-            if(map.containsKey(target - nums[i])){
-                return new int[]{map.get(target - nums[i]), i};
+            int diff = target - nums[i];
+            if(map.containsKey(diff)){
+                return new int[]{map.get(diff), i};
             }
-            map.put(nums[i], i);
+            map.put(nums[i],i);
         }
         
         return new int[]{-1,-1};
