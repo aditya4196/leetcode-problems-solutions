da4d14d682c1d4f74e74baf512b8d1c29a8d56c9
Aditya Jadhav <adityajadhav537@gmail.com>
2024-04-03 09:40:01 -0500

    Time: 160 ms (23.87%), Space: 62.6 MB (36.19%) - LeetHub

--- 2781-length-of-the-longest-valid-substring/2781-length-of-the-longest-valid-substring.java
+++ 2781-length-of-the-longest-valid-substring/2781-length-of-the-longest-valid-substring.java
@@ -2,37 +2,33 @@ class Solution {
     public int longestValidSubstring(String word, List<String> forbidden) {
         
         int n = word.length();
-        HashSet<String> forbiddenSet = new HashSet<String>(forbidden);
         int end = n-1;
         int maxLength = 0;
-        int i=0, j=0;
         
-        for(i=n-1; i>=0; i--){
-            for(j=i; ((j-i) < 10) && (j<=end); j++){
-                String toCheck = word.substring(i, j+1);
-                if(forbiddenSet.contains(toCheck)){
+        HashSet<String> forbiddenSet = new HashSet<String>(forbidden);
+        
+        for(int i=n-1; i>=0; i--){
+            for(int j=i; (j<=end && (j-i) <= 10); j++){
+                String subword = word.substring(i, j+1);
+                if(forbiddenSet.contains(subword)){
                     end = j-1;
                     break;
                 }
             }
-            
             maxLength = Math.max(maxLength, end-i+1);
         }
         
         return maxLength;
         
         
+        
+        
     }
 }
 
-
-/*
-
-cbaaaabc
- i
- j
-   e
-
-3
-
-*/
+// forbidden = 'aaa','cb'
+    
+// word = 'cbaaaabc'
+//                e
+//            i
+//                j
