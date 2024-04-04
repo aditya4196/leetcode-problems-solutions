4928033ef9e88aefa3aeb08f89046a8158656170
Aditya Jadhav <adityajadhav537@gmail.com>
2024-04-04 16:23:36 -0500

    Time: 28 ms (18.95%), Space: 45.2 MB (91.96%) - LeetHub

---
+++ 0621-task-scheduler/0621-task-scheduler.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+        
+        Map<Character, Integer> map = new HashMap();
+        for(char task : tasks){
+            map.put(task, map.getOrDefault(task,0)+1);
+        }
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>((a,b)->(b-a));
+        pq.addAll(map.values());
+        int result = 0;
+        
+        while(pq.size() > 0){
+            List<Integer> placed = new ArrayList<Integer>();
+            int cycle = n+1;
+            
+            for(int i=0; i<cycle; i++){
+                if(pq.size() > 0){
+                    placed.add(pq.poll());
+                }
+            }
+            
+            for(int freq : placed){
+                if(--freq > 0){
+                    pq.offer(freq);
+                }
+            }
+            
+            result += (pq.size() > 0)?cycle:placed.size();
+        }
+        
+        return result;
+        
+        
+        
+    }
+}
