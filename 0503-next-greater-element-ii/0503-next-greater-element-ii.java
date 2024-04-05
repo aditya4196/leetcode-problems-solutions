f0155bb296db1156630bf730043488a40a914d2f
Aditya Jadhav <adityajadhav537@gmail.com>
2024-04-05 16:16:07 -0500

    Time: 9 ms (89.84%), Space: 46.3 MB (23.49%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -1,44 +1,38 @@
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
-        
-        int[] result = new int[nums.length];
+        int n = nums.length;
+        Stack<Integer> stack = new Stack<Integer>();
+        int[] result = new int[n];
         Arrays.fill(result, -1);
-        Stack<Integer> stack = new Stack();
         
-        for(int i=0; i<nums.length; i++){
+        for(int i=0; i<n; i++){
             while(!stack.isEmpty() && nums[stack.peek()] < nums[i]){
                 result[stack.pop()] = nums[i];
             }
             stack.push(i);
         }
         
-        for(int j=0; j<nums.length; j++){
+        for(int j=0; j<n; j++){
             while(!stack.isEmpty() && nums[stack.peek()] < nums[j]){
                 result[stack.pop()] = nums[j];
             }
-            stack.push(j);   
         }
-        
         return result;
         
         
         
+        
     }
-    
-    
 }
 
 /*
 
+nums = [ 1, 2, 1]
+       [ 2,-1,-1]
+            
+       stack = [1,2]
 
-[1,2,1]
 
-[2,-1,-1]
 
-[100,1,11,1,120,111,123,1,-1,-100]
-                       i
-[120,11,120,120,123,123,-1,-1,-1,-1]
-
-stack = [123,1]
 
 */
