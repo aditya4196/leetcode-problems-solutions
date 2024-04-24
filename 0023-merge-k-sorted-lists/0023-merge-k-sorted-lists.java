1a0b8556f6f9e2e7be11b7ef8fcc9427dd7d3d01
Aditya Jadhav <adityajadhav537@gmail.com>
2024-04-24 14:35:45 -0400

    Time: 4 ms (73.03%), Space: 44.3 MB (62.15%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -11,41 +11,27 @@
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
         
-        PriorityQueue<ListNode> pq = new PriorityQueue<ListNode>((a,b) -> (a.val - b.val));
+        if(lists.length == 0) return null;
         
-        for(ListNode list: lists){
+        PriorityQueue<ListNode> pq = new PriorityQueue<ListNode>((a,b) -> (Integer.compare(a.val, b.val)));
+        
+        for(ListNode list : lists){
             if(list != null) pq.offer(list);
         }
         
         if(pq.size() == 0) return null;
         
-        ListNode result = new ListNode(-1);
-        ListNode dummy = result;
+        ListNode output = new ListNode(-1);
+        ListNode dummy = output;
         
         while(pq.size() > 0){
-            ListNode curr = pq.poll();
-            dummy.next = curr;
+            ListNode currPoll = pq.poll();
+            dummy.next = currPoll;
             dummy = dummy.next;
-            if(curr.next != null) pq.offer(curr.next);
+            if(currPoll.next != null) pq.offer(currPoll.next);
         }
         
-        return result.next;
-        
-        
+        return output.next;
         
     }
 }
-
-/*
-
-
-1 -> 4 -> 5
-1 -> 3 -> 4
-2 -> 6
-
-
-pq = [4,1,2]
-
-
-
-*/
