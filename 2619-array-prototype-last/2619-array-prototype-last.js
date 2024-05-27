a4c3180c1cb567b55350b8c3f51c8b92d58ea6cf
Aditya Jadhav <adityajadhav537@gmail.com>
2024-05-26 22:21:09 -0400

    Time: 50 ms (58.54%), Space: 48.6 MB (54.73%) - LeetHub

---
+++ 2619-array-prototype-last/2619-array-prototype-last.js
@@ -0,0 +1,13 @@
+/**
+ * @return {null|boolean|number|string|Array|Object}
+ */
+Array.prototype.last = function() {
+    const val = this.pop();
+    
+    return val === undefined ? -1 : val;
+};
+
+/**
+ * const arr = [1, 2, 3];
+ * arr.last(); // 3
+ */
