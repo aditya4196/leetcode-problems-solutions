475aab15311d0d8fe44fd29c2a86f6893bdf6d16
Aditya Jadhav <adityajadhav537@gmail.com>
2024-05-26 22:16:13 -0400

    Time: 57 ms (49.39%), Space: 51.5 MB (53.35%) - LeetHub

--- 2677-chunk-array/2677-chunk-array.js
+++ 2677-chunk-array/2677-chunk-array.js
@@ -7,13 +7,13 @@ var chunk = function(arr, size) {
     const chunked = [];
     let i=0;
     
-    for(i=0; i<=arr.length-size; i+=size){
+    for(i=0; i<arr.length; i+=size){
         chunked.push(arr.slice(i, i+size));
     }
     
-    if(arr.length%size != 0){
-        chunked.push(arr.slice(i));
-    }
+//     if(arr.length%size != 0){
+//         chunked.push(arr.slice(i));
+//     }
     
     return chunked;
     
